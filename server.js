4daa3446b14daed7deb9e3ad75052c0ed2978335
henry hawm <henry.hawm@gmail.com>
2020-02-18 09:26:25 -0600

    found the error

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 // MongoDB_URI reference
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
