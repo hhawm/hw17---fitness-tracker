41d4d61096c858b1f03b41fbe6022d0712bf76e7
henry hawm <henry.hawm@gmail.com>
2020-02-18 00:15:17 -0600

    trying

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,42 +2,26 @@
 // Models
 var db = require("../models");
 
-String.prototype.toObjectId = function () {
-    var ObjectId = require("mongoose").Types.ObjectId;
-    return new ObjectId(this.toString());
-};
-
-// Routes
+// Routes 
 module.exports = function (app) {
-    // Creates workouts
-    db.Workout.create({ name: "Workouts" })
-        .then(dbWorkout => {
-            console.log(dbWorkout);
-        })
-        .catch(({ message }) => {
-            console.log(message);
-        });
-    //To get All Workouts
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({})
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.json(err);
-            });
-    });
-
-    // Creates new workout
-    app.post("/api/workouts", function (req, res) {
-        db.Workout.create(req.body).then(dbWorkout => {
+    // Get route for getting all workouts
+    app.get("/api/workouts", function (req, res) {
+        // getting it out of Workout.js in models.
+        db.Workout.find({}).then(function (dbWorkout) {
             res.json(dbWorkout);
         }).catch(err => {
             res.json(err);
         })
     })
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    })
 
-    // Find workout by Id
+    // Get single workout by id#
     app.get("/api/workouts/:id", function (req, res) {
         var id = req.params.id;
         db.Workout.findById(id, function (err, dbWorkout) {
@@ -48,35 +32,26 @@ module.exports = function (app) {
         })
     })
 
-    // FindoneAndupdate will Find Id first, then Update
-    app.put("/api/workouts/:id", (req, res) => {
-        console.log(req.body);
-        db.Workout.findOneAndUpdate(
-            { _id: req.params.id },
-            {
-                $push: {
-                    exercises: req.body
-                },
-            },
-            { new: true })
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-                console.log(dbWorkout)
-            }).catch(err => {
-                res.json(err);
-            });
-    });
+    // Post new workout in db
+    app.post("/api/workouts/", function (req, res) {
+        db.Workout.create({ exercise: req.body }).then(function (dbWorkout) {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
+    })
 
-    // Get route for all workouts
-    app.get("/api/workouts/range", (req, res) => {
-        db.Workout.find({})
-            .populate("exercises")
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
+    // Find and update a workout with another exercise in db by id
+    app.put("/api/workouts/:id", function (req, res) {
+        var query = { _id: req.params.id };
+        db.Workout.findOneAndUpdate(query, {
+            $push: { exercises: [req.body] }
+        }, function (err, dbWorkout) {
+            if (err) {
                 res.json(err);
-            });
-    });
-};
-
+            } else {
+                res.json(dbWorkout);
+            }
+        })
+    })
+}
